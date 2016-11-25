2c9676cc8a48955e30efaa2f8a9bfe8278cd8481
Taraxippus <taraxippussan@gmail.com>
2016-11-25 20:25:48 +0100

    initial commit

--- app/src/main/java/com/taraxippus/hotaru/MainActivity.java
+++ app/src/main/java/com/taraxippus/hotaru/MainActivity.java
@@ -24,8 +24,8 @@ public class MainActivity extends Main
 	public void initPasses()
 	{
 		passes[0] = PASS_SCENE = new Pass(this, com.taraxippus.yui.R.raw.vertex_scene, com.taraxippus.yui.R.raw.fragment_scene, new String[] { "a_Position" },  new int[] { 3 }, new String[] { "u_MVP" });
-		passes[1] = PASS_BLOOM = new Pass.Bloom();
-		passes[2] = new Pass.Bloom();
+		//passes[1] = PASS_BLOOM = new Pass.Bloom();
+		//passes[2] = new Pass.Bloom();
 		passes[3] = new Pass.DefaultPost(this, renderer.width, renderer.height, 0.8F, 0.2F, new int[0]);
 	}
 
